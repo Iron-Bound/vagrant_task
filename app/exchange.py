230e430d0c59d0d3fee8f9720da4af236a35068b
Andrew Boundy <andrew.mrboundy@gmail.com>
2017-08-26 16:01:27 +1000

    init page formating

--- app/exchange.py
+++ app/exchange.py
@@ -1,13 +1,16 @@
 import sqlite3
-from bottle import route, run, debug
+from bottle import route, run, debug, template
 
 @route('/')
 @route('/rates')
-def main_page():
+def main_page(): # Get current coin rates
   con = sqlite3.connect('data.db')
   c = con.cursor()
   c.execute("SELECT name, rate FROM exchange")
-  res = c.fetchall()
-  return str(res)
+  res = c.fetchall() # coin rate lookup
+  c.close()
+  page = template('rates_page', rows=res) # return page
+  return page
 
+debug(True)
 run(reloader=True)
---
+++ app/rates_page.tpl
@@ -0,0 +1,11 @@
+%#coin rates page
+<p>Daily Crypto Currency Rates:</p>
+<table border="1">
+%for row in rows:
+  <tr>
+  %for col in row:
+    <td>{{col}}</td>
+  %end
+  </tr>
+%end
+</table>
